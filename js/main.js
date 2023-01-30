7905db1781df874c53dc3f329694f01d62ba26a4
abdulazizpro378 <abdujazizabdi045@gmail.com>
2023-01-30 07:02:31 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const sidebar = document.querySelector("aside");
+const sidebarToggle = document.querySelector(".hamburger");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
